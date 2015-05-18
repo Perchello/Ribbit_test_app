bf403a73990736a03b9a20653d41152de1083f74
Perchello <Perchello@users.noreply.github.com>
2015-05-19 00:11:47 +0100

    Added function to make vide, photo or to pick one

--- app/src/main/java/perchello/ribbit/LoginActivity.java
+++ app/src/main/java/perchello/ribbit/LoginActivity.java
@@ -104,11 +104,6 @@ public class LoginActivity extends ActionBarActivity {
 		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_log_in, menu);
-		return true;
-	}
+
 
 }
--- app/src/main/java/perchello/ribbit/MainActivity.java
+++ app/src/main/java/perchello/ribbit/MainActivity.java
@@ -1,6 +1,9 @@
 package perchello.ribbit;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -45,6 +48,7 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
 
     public static final int MEDIA_TYPE_IMAGE = 4;
     public static final int MEDIA_TYPE_VIDEO = 5;
+    public static final int FILE_SIZE_LIMIT = 1024*1024*10;
 
     protected Uri mMediaUri;
 
@@ -67,10 +71,29 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
                         }
                         break;
                     case 1: // Take video
+                        Intent videoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                        mMediaUri = getOutputMEdiaFileUri(MEDIA_TYPE_VIDEO);
+                        if (mMediaUri==null){
+                            Toast.makeText(MainActivity.this, R.string.error_external_storage, Toast.LENGTH_LONG).show();
+
+                        }
+                        else {
+                            videoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+                            videoIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 10);
+                            videoIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 0);
+                            startActivityForResult(videoIntent, TAKE_VIDEO_REQUEST);
+                        }
                         break;
                     case 2: // Choose picture
+                        Intent choosePhotoIntent = new Intent (Intent.ACTION_GET_CONTENT);
+                        choosePhotoIntent.setType ("image/*");
+                        startActivityForResult(choosePhotoIntent, PICK_PHOTO_REQUEST);
                         break;
                     case 3: // Choose video
+                        Intent chooseVideoIntent = new Intent (Intent.ACTION_GET_CONTENT);
+                        chooseVideoIntent.setType ("video/*");
+                        Toast.makeText(MainActivity.this, getString(R.string.video_file_size_warning), Toast.LENGTH_LONG).show();;
+                        startActivityForResult(chooseVideoIntent, PICK_VIDEO_REQUEST);
                         break;
                 }
             }
@@ -188,7 +211,64 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
 		}
 	}
 
-	private void navigateToLogin() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+            if (requestCode == PICK_PHOTO_REQUEST || requestCode == PICK_VIDEO_REQUEST) {
+                    if (data == null) {
+                        Toast.makeText(this, getString(R.string.general_error), Toast.LENGTH_LONG).show();;
+                    }
+                    else {
+                        mMediaUri = data.getData();
+                    }
+                Log.i (TAG, "MEdia URI: " + mMediaUri);
+                    if (requestCode == PICK_VIDEO_REQUEST) {
+                        //make sure less than 10 MB
+                        int fileSize = 0;
+
+                        InputStream inputStream = null;
+                        try {
+                            inputStream = getContentResolver().openInputStream(mMediaUri);
+                            fileSize = inputStream.available();
+
+                        } catch (FileNotFoundException e) {
+                            Toast.makeText(this, getString(R.string.error_opening_file), Toast.LENGTH_LONG).show();;
+                            return;
+                        } catch (IOException e) {
+                            Toast.makeText(this, getString(R.string.error_opening_file), Toast.LENGTH_LONG).show();;
+                            return;
+                         }
+                        finally {
+                            try {
+                                inputStream.close();
+                            } catch (IOException e) { }
+                        }
+                        if (fileSize >= FILE_SIZE_LIMIT){
+                            Toast.makeText(this, getString(R.string.error_file_size_too_large), Toast.LENGTH_LONG).show();;
+                            return;
+                        }
+
+                    }
+
+            }
+            else {
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                mediaScanIntent.setData(mMediaUri);
+                sendBroadcast(mediaScanIntent);
+            }
+            Intent recipientsIntent = new Intent(this, RecipientsActivity.class);
+            recipientsIntent.setData(mMediaUri);
+            startActivity(recipientsIntent);
+        }
+        else if (resultCode!= RESULT_CANCELED){
+            Toast.makeText (this, R.string.general_error, Toast.LENGTH_LONG).show();;
+
+        }
+    }
+
+    private void navigateToLogin() {
 		Intent intent = new Intent(this, LoginActivity.class);
 		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
